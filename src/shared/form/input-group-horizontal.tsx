0242f5de859bb357134bb00867b4ad05642f48f6
Nick Rimmer <xan@dipteam.com>
2021-08-28 22:07:14 +0200

    send binary data

--- src/shared/form/input-group-horizontal.tsx
+++ src/shared/form/input-group-horizontal.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import {Col, Form, Row} from "react-bootstrap";
 
 type Props = {
-  value?: string | number | boolean | undefined;
   defaultValue?: string | number | boolean | undefined;
   controlId: string;
   label: string;
@@ -10,20 +9,19 @@ type Props = {
   type?: "text" | "password";
 };
 
-const Result = ({controlId, label, placeholder, defaultValue, type = "text", value}: Props) => {
+const Result = ({controlId, label, placeholder, defaultValue, type = "text"}: Props) => {
   let inputProps = ({
     placeholder,
     type
   });
 
   if (defaultValue) inputProps = ({...inputProps, ...{defaultValue: defaultValue.toString()}});
-  if (value) inputProps = ({...inputProps, ...{value: value.toString()}});
 
   return (
     <Form.Group as={Row} className="mb-3" controlId={controlId}>
       <Form.Label column md="3" lg="2">{label}</Form.Label>
       <Col>
-        <Form.Control {...inputProps}/>
+        <Form.Control {...inputProps} />
       </Col>
     </Form.Group>
   );
