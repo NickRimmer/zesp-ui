59d8e24ad0ec49d15862636940b5cc284cbeeaf9
Nick Rimmer <xan@dipteam.com>
2021-09-11 00:17:43 +0200

    replace image if not loaded

--- src/pages/devices/item-image.tsx
+++ src/pages/devices/item-image.tsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {DeviceListItem} from "../../models/DeviceListItem";
 
 interface IProps {
   device: DeviceListItem
 }
 
+const defaultImage = "/device-images/default.png";
+
 export default (props: IProps) => {
-  return (<div className="item-image" style={{backgroundImage: `url(${props.device.image})`}}/>);
+  const [image, setImage] = useState(props.device.image);
+
+  useEffect(() => {
+    const testImage = new Image();
+    testImage.onerror = () => {
+      setImage(defaultImage);
+    }
+    testImage.src = image;
+  }, []);
+
+  return (<div className="item-image" style={{backgroundImage: `url(${image})`}}/>);
 }
