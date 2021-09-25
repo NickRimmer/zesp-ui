53dbbd8945e8477039e572624eb6d9ee57973c7f
Nick Rimmer <NickRimmer@users.noreply.github.com>
2021-09-25 03:24:46 +0200

    Features/refactoring (#33)
    * self-hosted support
    
    * new pipeline for self-hosted version

--- src/loader.js
+++ src/loader.js
@@ -1,6 +1,6 @@
-window.$ = window.jQuery = require('jquery')
+// window.$ = window.jQuery = require('jquery')
 
 if (process.env.NODE_ENV !== "development") console.debug = () => {
-    // show info for dev environment only
-    console.log("Debug logs are disabled");
+  // show info for dev environment only
+  // console.log("Debug logs are disabled");
 };
--- src/shared/form/react-form/logic.tsx
+++ src/shared/form/react-form/logic.tsx
@@ -1,5 +1,4 @@
 import {FormEvent} from "react";
-import $ from "jquery";
 
 // we need this 'any' type to provide general solution
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -9,23 +8,25 @@ const Result = {
   handleSubmit: function handleSubmit(e: FormEvent, action: IAction) {
     e.preventDefault();
 
-    // we need this 'any' type to provide general solution
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const data: any = {};
+    // // we need this 'any' type to provide general solution
+    // // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    // const data: any = {};
+    //
+    // for (const input of $(e.target).find("input")) {
+    //   const id = $(input).prop("id");
+    //   if (id == null) return;
+    //
+    //   if ($(input).prop("type") === "checkbox") {
+    //     if ($(input).prop("checked") === true) data[id] = $(input).attr("checked_value") || "true";
+    //     else data[id] = $(input).attr("unchecked_value") || "false";
+    //   } else {
+    //     data[id] = $(input).val();
+    //   }
+    // }
+    // // console.log(data);
+    // action(data)
 
-    for (const input of $(e.target).find("input")) {
-      const id = $(input).prop("id");
-      if (id == null) return;
-
-      if ($(input).prop("type") === "checkbox") {
-        if ($(input).prop("checked") === true) data[id] = $(input).attr("checked_value") || "true";
-        else data[id] = $(input).attr("unchecked_value") || "false";
-      } else {
-        data[id] = $(input).val();
-      }
-    }
-    // console.log(data);
-    action(data)
+    console.warn("Not implemented yet")
   }
 };
 
